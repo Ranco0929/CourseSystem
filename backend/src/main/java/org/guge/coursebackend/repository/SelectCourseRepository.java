3a97f3bc06dff83d7ac607fdbe7dccae1b1e4737
Hitrel <hitrel@outlook.com>
2020-05-26 12:25:36 +0800

    change maven jdk version to 14  update TeachCourseRepo and SelectCourseRepo select operate to native query

--- backend/src/main/java/org/guge/coursebackend/repository/SelectCourseRepository.java
+++ backend/src/main/java/org/guge/coursebackend/repository/SelectCourseRepository.java
@@ -2,6 +2,7 @@ package org.guge.coursebackend.repository;
 
 import org.guge.coursebackend.entity.SelectCourse;
 import org.guge.coursebackend.entity.subentity.SelectCourseKey;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface SelectCourseRepository extends CrudRepository<SelectCourse, SelectCourseKey> {
 
+    @Query(value = "SELECT * FROM select_course WHERE user_id = ?1", nativeQuery=true)
     List<SelectCourse> findAllByUserId(Long userId);
 }
--- backend/src/main/java/org/guge/coursebackend/repository/TeachCourseRepository.java
+++ backend/src/main/java/org/guge/coursebackend/repository/TeachCourseRepository.java
@@ -2,6 +2,7 @@ package org.guge.coursebackend.repository;
 
 import org.guge.coursebackend.entity.TeachCourse;
 import org.guge.coursebackend.entity.subentity.TeachCourseKey;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,6 @@ import java.util.List;
 @Repository
 public interface TeachCourseRepository extends CrudRepository<TeachCourse, TeachCourseKey> {
 
+    @Query(value = "SELECT * FROM teach_course WHERE course_id = ?1", nativeQuery=true)
     List<TeachCourse> findAllByCourseId(Long courseId);
 }
