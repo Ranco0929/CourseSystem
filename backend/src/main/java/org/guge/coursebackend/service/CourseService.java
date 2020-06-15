2e5242038eb757f59f6b909d2d025b2e0c053c78
Hitrel <hitrel@outlook.com>
2020-06-15 10:24:29 +0800

    修正Bug: CourseService逻辑错误

--- backend/src/main/java/org/guge/coursebackend/service/CourseService.java
+++ backend/src/main/java/org/guge/coursebackend/service/CourseService.java
@@ -3,6 +3,8 @@ package org.guge.coursebackend.service;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.guge.coursebackend.entity.Course;
+import org.guge.coursebackend.entity.SelectCourse;
+import org.guge.coursebackend.entity.TeachCourse;
 import org.guge.coursebackend.entity.subentity.SelectCourseKey;
 import org.guge.coursebackend.entity.subentity.TeachCourseKey;
 import org.guge.coursebackend.repository.CourseRepository;
@@ -86,20 +88,26 @@ public class CourseService {
         }
     }
 
-    public Result create(String coursename, String info, String avatar, Long creator, JSONArray teachers) {
+    public Result create(String courseName, String info, String avatar, Long creator, JSONArray teachers) {
         try {
             var course = new Course();
-            var it = courseRepository.findByName(coursename);
+            var it = courseRepository.findByName(courseName);
 
             if (it.isEmpty()) {
-                course.setName(coursename);
+                course.setName(courseName);
                 course.setAvatar(avatar);
                 course.setInfo(info);
                 course.setCreator(creator);
                 course.setCreatedAt(new Date());
 
-                course.setTeachers(teachers.toJavaList(Long.class));
+                var teacherList = teachers.toJavaList(Long.class);
+
+                course.setTeachers(teacherList);
                 courseRepository.save(course);
+
+                var existsCourse = courseRepository.findByName(courseName).orElseThrow();
+
+                addTeachCourseRecord(course.getCourseId(), course.getTeachers());
 
                 return ResultFactory.buildSuccessResult("success");
             } else {
@@ -110,19 +118,39 @@ public class CourseService {
         }
     }
 
-    public Result add(long cId,long uId){
+    private void addTeachCourseRecord(long courseId, List<Long> teachers) throws Exception {
+        for (var teacher : teachers) {
+            var key = new TeachCourseKey(teacher, courseId);
+
+            var new_record = new TeachCourse();
+            new_record.setTeachCourseKey(key);
+
+            new_record.setCreatedAt(new Date());
+            new_record.setUpdatedAt(new Date());
+            new_record.setEnable(true);
+
+            teachCourseRepository.save(new_record);
+        }
+    }
+
+    public Result add(long courseId,long userId){
         try{
-            var itcourse = courseRepository.findById(cId);
-            var ituser = userRepository.findById(uId);
-            if(!itcourse.isEmpty()){
-                var courseId = itcourse.get().getCourseId();
-                var userId = ituser.get().getUserId();
-                itcourse.get().setCourseId(courseId);
-                itcourse.get().setCreatedAt(new Date());
-                courseRepository.save(itcourse.get());
-                return ResultFactory.buildSuccessResult("success");
-            }else{
+            var courseIt = courseRepository.findById(courseId);
+            var userIt = userRepository.findById(userId);
+            if (courseIt.isEmpty() || userIt.isEmpty()) {
                 return ResultFactory.buildFailResult("wrong course id or user id");
+            }else{
+               var key = new SelectCourseKey(userId, courseId);
+               var new_record = new SelectCourse();
+
+               new_record.setSelectCourseKey(key);
+               new_record.setCreatedAt(new Date());
+               new_record.setUpdatedAt(new Date());
+               new_record.setEnable(true);
+
+               selectCourseRepository.save(new_record);
+
+               return ResultFactory.buildSuccessResult("success");
             }
         }catch (Exception e){
             return ResultFactory.buildFailResult(e.getMessage());
@@ -131,12 +159,18 @@ public class CourseService {
 
     public Result delete(long courseId, long userId,String role) {
         try {
-            var it = courseRepository.findById(courseId);
-            var user = userRepository.findById(userId).orElseThrow();
+            var course_it = courseRepository.findById(courseId);
+            var user_it = userRepository.findById(userId);
+
+            if (course_it.isEmpty() || user_it.isEmpty()) {
+                return ResultFactory.NotFoundResult("wrong userId or courseId");
+            }
+            var user = user_it.get();
             switch (user.getRole()) {
-                case STUDENT -> deleteselectCourse(userId,courseId);
+                case STUDENT -> deleteSelectCourse(userId,courseId);
                 case TEACHER -> {
-                    deleteTeachCourse(userId, courseId);
+                    deleteSelectCourse(courseId);
+                    deleteTeachCourse(courseId);
                     deleteCourse(courseId);
                 }
             }
@@ -146,30 +180,44 @@ public class CourseService {
         }
     }
 
-    private void deleteTeachCourse(long userId, long courseId) throws Exception {
-        var key = new TeachCourseKey(userId, courseId);
-        var it = teachCourseRepository.findById(key);
-        var tempteachCourse = it.get();
-        tempteachCourse.setUpdatedAt(new Date());
-        tempteachCourse.setEnable(false);
-        teachCourseRepository.save(tempteachCourse);
+    private void deleteTeachCourse(long courseId) throws Exception {
+        var teachRecords = teachCourseRepository.findAllByCourseId(courseId);
+
+        for (var record : teachRecords) {
+            record.setEnable(false);
+            record.setUpdatedAt(new Date());
+
+            teachCourseRepository.save(record);
+        }
     }
 
-    private void deleteselectCourse(long userId, long courseId) throws  Exception{
+    private void deleteSelectCourse(long courseId) throws Exception {
+        var selectRecords = selectCourseRepository.findAllByCourseId(courseId);
+
+        for (var record : selectRecords) {
+            record.setEnable(false);
+            record.setUpdatedAt(new Date());
+
+            selectCourseRepository.save(record);
+        }
+    }
+
+    private void deleteSelectCourse(long userId, long courseId) throws  Exception{
         var key = new SelectCourseKey(userId, courseId);
-        var it = selectCourseRepository.findById(key);
-        var tempselectCourse = it.get();
-        tempselectCourse.setUpdatedAt(new Date());
-        tempselectCourse.setEnable(false);
-        selectCourseRepository.save(tempselectCourse);
+        var record = selectCourseRepository.findById(key).orElseThrow();
+
+        record.setEnable(false);
+        record.setUpdatedAt(new Date());
+        selectCourseRepository.save(record);
     }
 
-    private void deleteCourse(long courseId){
-        var it = courseRepository.findById(courseId);
-        var tempcourse = it.get();
-        tempcourse.setUpdatedAt(new Date());
-        tempcourse.setEnable(false);
-        courseRepository.save(tempcourse);
+    private void deleteCourse(long courseId) throws Exception{
+        var course = courseRepository.findById(courseId).orElseThrow();
+
+        course.setEnable(false);
+        course.setUpdatedAt(new Date());
+
+        courseRepository.save(course);
     }
 
     public Result update(Course course) {
@@ -179,9 +227,11 @@ public class CourseService {
             if (it.isEmpty()) {
                 return ResultFactory.buildFailResult("No Specified course by id: " + course.getCourseId());
             } else {
+
                 course.setCreatedAt(it.get().getCreatedAt());
                 course.setUpdatedAt(new Date());
                 courseRepository.save(course);
+
                 return ResultFactory.buildSuccessResult("success");
             }
         } catch (Exception e) {
@@ -191,13 +241,18 @@ public class CourseService {
 
     public Result getCourse(long courseId) {
         try {
-            var itcourse = courseRepository.findById(courseId);
-            var Id = itcourse.get().getCourseId();
-            if (itcourse.isEmpty()) {
+            var course_it = courseRepository.findById(courseId);
+
+            if (course_it.isEmpty()) {
                 return ResultFactory.NotFoundResult("wrong course id");
             } else {
-                    var new_course = courseRepository.findById(courseId).orElseThrow();
-                    return ResultFactory.buildSuccessResult(new_course);
+                    var course = course_it.get();
+
+                    if (!course.isEnable()) {
+                        return ResultFactory.buildFailResult("course has been delete");
+                    }
+
+                    return ResultFactory.buildSuccessResult(course);
             }
             }catch(Exception e){
                 return ResultFactory.ServerErrorResult(e.getMessage());
@@ -213,10 +268,16 @@ public class CourseService {
                 var studentlist = teachCourseRepository.findAllByCourseId(course.getSelectCourseKey().getCourseId());
                 for (var student :
                         studentlist) {
+                    if (!student.isEnable()) {
+                        continue;
+                    }
                     var studentId = student.getTeachCourseKey().getUserId();
+
                     if (!result.containsKey(studentId)) {
                         JSONObject keyValue = new JSONObject();
-                        var new_student = userRepository.findById(studentId).get();
+
+                        var new_student = userRepository.findById(studentId).orElseThrow();
+
                         keyValue.put("userId", new_student.getUserId());
                         keyValue.put("name", new_student.getName());
                         keyValue.put("info", new_student.getInfo());
